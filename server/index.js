98e69553e1abd4677d54d3de3202126c220f554f
Kim <a01088570194@gmail.com>
2019-04-04 19:08:29 +0900

    Feat: add gitignore

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const volleyball = require('volleyball')
+
+const app = express();
+
+app.use(volleyball);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello Word!'
+    })
+})
+
+function notFound(req, res, next){
+    res.status(404);
+    const error = new Error('Not Found - ' + req.originalUrl);
+    next(error);
+}
+
+function errorHandler(err, req, res, next){
+    res.status(res.statusCode || 500);
+    res.json({
+        message: err.message,
+        stack: err.stack
+    })
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Listening on port', port);
+});

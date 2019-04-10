be8c2e3f5a7b1981599098e1955f5465e895ac24
HTMLhead <mrdoo309@gmail.com>
2019-04-10 17:46:09 +0900

    Feat: 로그인 기능 제작 완료

--- src/component/user/SignIn.jsx
+++ src/component/user/SignIn.jsx
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { SignInForm, SignNav, SignLogo, Container } from './SignForm.jsx';
+import useFetch from '../fetch.js';
 
 const SignIn = props => {
+  const [id, setId] = useState({ b: false, data: '' });
+  const [pw, setPw] = useState({ b: false, data: '' });
+  const [submitBtn, setSubmitBtn] = useState({
+    bLoading: false,
+    bCorrect: true,
+  });
+
+  const getId = e => {
+    const curVal = e.target.value;
+    setId({ b: true, data: curVal });
+  };
+
+  const getPw = e => {
+    const curVal = e.target.value;
+    setPw({ b: true, data: curVal });
+  };
+
+  const submit = async e => {
+    if (!(id.b && pw.b)) {
+      setSubmitBtn({ bLoading: false, bCorrect: false });
+      return;
+    }
+    console.log(id.data, pw.data)
+    setSubmitBtn({ bLoading: true, bCorrect: true });
+    const jsonHeader = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+
+    const body = {
+      userid: id.data,
+      password: pw.data,
+    };
+
+    const res = await useFetch(
+      'https://hea-b.herokuapp.com/users/signin',
+      'POST',
+      jsonHeader,
+      JSON.stringify(body)
+    );
+
+    if(res.users.length) {
+      window.location.replace('http://localhost:3000');
+    } else {
+      setSubmitBtn({bLoading: false, bCorrect: false})
+    } 
+  };
   return (
     <>
       <SignLogo as={Link} to="/" />
       <Container>
-        <SignInForm />
+        <SignInForm Fns={{ getId, getPw, submit }} Datas={{ id, pw, submitBtn }} />
         <SignNav />
       </Container>
     </>

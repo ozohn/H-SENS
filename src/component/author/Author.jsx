0941c6075f6776212ba1f68c7a5358dfc2d2f114
HTMLhead <mrdoo309@gmail.com>
2019-04-22 17:13:22 +0900

    Feat: 데이터를 받아와 작가리스트 생성기능 제작

--- src/component/author/Author.jsx
+++ src/component/author/Author.jsx
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import useFetch from '../fetch.js';
 import { Grid, Image } from 'semantic-ui-react';
 
-const ListItems = styled.div`
-  height: 300px;
-  width: 100%;
-  display: flex;
-  margin: 0 auto;
+const AuthorDesc = styled.div`
+  border-top: 1px solid #393e46;
+  magin: auto;
 `;
-const Item = styled.div`
-  flex: 1;
-  border: 1px solid #bbb;
+const AuthorName = styled.div`
+  border-top: 1px solid #393e46;
+  magin: auto;
+`;
+const Container = styled.div`
+  border: 1px solid #393e46;
 `;
 
+const Authors = props => {
+  if (props.user === undefined) {
+    return (
+      <Container>
+        <Image />
+        <AuthorName />
+        <AuthorDesc />
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Image src={props.user.userimage} />
+      <AuthorName>{props.user.username}</AuthorName>
+      <AuthorDesc>{props.user.userdesc}</AuthorDesc>
+    </Container>
+  );
+};
+
+const getUserData = async () => {
+  return await useFetch(`${process.env.REACT_APP_SERVER_URL}/main/users`, 'POST');
+};
+
 export default function Author() {
+  const [authorData, setAuthorData] = useState('');
+
+  useEffect(() => {
+    const fetchedData = getUserData();
+    fetchedData.then(user => {
+      // user.length = authors;
+      setAuthorData(user);
+    });
+  }, []);
+
   return (
     <Grid columns={3} stackable>
       <Grid.Column>
-        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+        <Authors user={authorData[1]} />
       </Grid.Column>
       <Grid.Column>
-        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+        <Authors user={authorData[2]} />
       </Grid.Column>
       <Grid.Column>
-        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+        <Authors user={authorData[3]} />
       </Grid.Column>
     </Grid>
   );

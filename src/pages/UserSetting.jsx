ca894e691afb5fd7ab874e44685b893ad9480461
pdvonzoo <vonzoo@naver.com>
2019-04-23 15:51:06 +0900

    Fix: code review 반영

--- src/pages/UserSetting.jsx
+++ src/pages/UserSetting.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Form } from 'semantic-ui-react';
-import styled from 'styled-components';
+import { Form } from "semantic-ui-react";
+import styled from "styled-components";
 
 const FormContainer = styled.div`
   padding-left: 8rem;
@@ -8,38 +8,54 @@ const FormContainer = styled.div`
 `;
 
 function UserSetting({ user, setUser }) {
-  const [userName, setUserName] = useState('');
-  const [userDesc, setUserDesc] = useState('');
-  const [userImage, setUserImage] = useState('');
+  const [userName, setUserName] = useState("");
+  const [userDesc, setUserDesc] = useState("");
+  const [userImage, setUserImage] = useState("");
 
   useEffect(() => {
     setUser({
-      username: userName?userName:user.username, 
-      userdesc: userDesc?userDesc:user.userdesc,  
-      userimage: userImage?userImage:user.userimage })
-  }, [userName, userDesc, userImage])
+      username: userName ? userName : user.username,
+      userdesc: userDesc ? userDesc : user.userdesc,
+      userimage: userImage ? userImage : user.userimage
+    });
+  }, [userName, userDesc, userImage]);
 
   return (
     <FormContainer>
       <Form>
-        <Form.Input label='user name' placeholder='name' onChange={(e) => {setUserName(e.target.value);} }/>
-        <Form.Field label='user description' control='textarea' rows='3' onChange={(e) => setUserDesc(e.target.value)} />
+        <Form.Input
+          label="user name"
+          placeholder="name"
+          onChange={e => {
+            setUserName(e.target.value);
+          }}
+        />
+        <Form.Field
+          label="user description"
+          control="textarea"
+          rows="3"
+          onChange={e => setUserDesc(e.target.value)}
+        />
       </Form>
       <div>
-        <input type="file" accept=".jpg, .jpeg, .png" onChange={e => getBase64(e.target.files[0], setUserImage)} />
+        <input
+          type="file"
+          accept=".jpg, .jpeg, .png"
+          onChange={e => getBase64(e.target.files[0], setUserImage)}
+        />
       </div>
     </FormContainer>
-  )
+  );
 }
 
 function getBase64(file, setUserImage) {
-  var reader = new FileReader();
+  const reader = new FileReader();
   reader.readAsDataURL(file);
-  reader.onload = function () {
+  reader.onload = function() {
     setUserImage(reader.result);
   };
-  reader.onerror = function (error) {
-    console.error('Error: ', error);
+  reader.onerror = function(error) {
+    console.error("Error: ", error);
   };
 }
 

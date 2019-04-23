312ddce189d16fe1d00e0d773889c12ed7795fcb
HTMLhead <mrdoo309@gmail.com>
2019-04-23 16:52:46 +0900

    Refactor: main페이지가 로딩될 때 fetch요청을 받아오도록 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import List from '../component/list/List.jsx';
 import Author from '../component/author/Author.jsx';
 import Header from '../header.js';
+import useFetch from '../component/fetch.js';
 
-class MainPage extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <Author />
-        <List />
-      </>
-    );
-  }
-}
+const getUserData = async () => {
+  return await useFetch(`${process.env.REACT_APP_SERVER_URL}/main/users`, 'POST');
+};
+
+const MainPage = props => {
+  const [authorData, setAuthorData] = useState('');
+
+  useEffect(() => {
+    const fetchedData = getUserData();
+    fetchedData.then(user => {
+      setAuthorData(user);
+    });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Author authors={authorData} />
+      <List />
+    </>
+  );
+};
 
 export default MainPage;

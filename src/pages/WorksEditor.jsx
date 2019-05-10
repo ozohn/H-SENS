7f7579e8b6e7a07ba7d716812501df3926567b1c
pdvonzoo <vonzoo@naver.com>
2019-05-10 15:25:33 +0900

    Feat: work edit 폼 수정

--- src/pages/WorksEditor.jsx
+++ src/pages/WorksEditor.jsx
@@ -6,31 +6,71 @@ import { WorkContext } from '../context/work/workContext';
 import InputForm from '../component/form/Input';
 import TuiEditor from '../component/editor/Editor';
 
+const Container = styled.div`
+  width: 60vw;
+  padding: 4rem 4rem 4rem;
+`;
+
+const Field = styled.div`
+  margin-bottom: 7rem;
+  border: ${props => (props.editor ? '0.2rem solid #231f20' : 0)};
+`;
+
 const Button = styled(Link)`
-  margin-top: 2rem;
+  color: #95bfb4;
+  margin-left: 25rem;
+  display: inline-block;
+  vertical-align: bottom;
   border: 0;
   outline: none;
-  font-size: 2.2rem;
-  background-color: transparent;
-  color: #ff4d4d;
   cursor: pointer;
+  font-size: 3rem;
+  font-weight: bold;
+  color: #231f20;
+  background-color: #55fe47;
   &:hover {
-    border-bottom: 1px solid #ff4d4d;
-  }
-  &&& {
-    float: right;
+    color: #231f20;
   }
 `;
 const Input = styled.input`
-  width: 50vw;
-  margin: 4rem 0 4rem 0;
+  display: inline-block;
+  margin-left: 4rem;
+  padding-left: 1rem;
+  width: 22rem;
   border: 0;
-  border-bottom: 0.2rem solid #95bfb4;
+  border-bottom: 0.2rem solid #231f20;
   outline: 0;
   font-size: 4rem;
   font-weight: bold;
   background: transparent;
   color: #1f272f;
+  vertical-align: top;
+  &:focus {
+    border-bottom: 0.2rem solid #55fe47;
+  }
+`;
+const InputFile = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  outline: 0;
+  opacity: 0;
+  pointer-events: none;
+  user-select: none;
+`;
+const FileLabel = styled.label`
+  padding: 3rem 0 2.4rem;
+  position: relative;
+  display: inline-block;
+  width: 14rem;
+  border: 2px solid #231f20;
+  background-color: #231f20;
+  font-size: 2rem;
+  font-weight: bold;
+  color: #54ff47;
+  transition: border 300ms ease;
+  cursor: pointer;
+  text-align: center;
 `;
 
 function WorksEditor({ location }) {
@@ -41,7 +81,28 @@ function WorksEditor({ location }) {
   const [worktitle, setWorktitle] = useState(work ? work.worktitle : '');
 
   return (
-    <>
+    <Container>
+      <Field>
+        <InputForm
+          Tag={Input}
+          cb={setWorktitle}
+          placeholder="Name"
+          label="Title"
+          type="text"
+          value={work ? work.worktitle : ''}
+        />
+      </Field>
+      <Field editor="editor">
+        <TuiEditor targetRef={workdesc} initialValue={work ? work.workdesc : ''} />
+      </Field>
+      <FileLabel>
+        Image
+        <InputFile
+          type="file"
+          accept=".jpg, .jpeg, .png"
+          onChange={e => getBase64(e.target.files[0], setWorkimage)}
+        />
+      </FileLabel>
       <Button
         to="/user"
         onClick={() => {
@@ -60,17 +121,7 @@ function WorksEditor({ location }) {
       >
         {submit}
       </Button>
-      <InputForm
-        Tag={Input}
-        cb={setWorktitle}
-        placeholder="Name"
-        label="Title"
-        type="text"
-        value={work ? work.worktitle : ''}
-      />
-      <input type="file" onChange={e => getBase64(e.target.files[0], setWorkimage)} />
-      <TuiEditor targetRef={workdesc} initialValue={work ? work.workdesc : ''} />
-    </>
+    </Container>
   );
 }
 

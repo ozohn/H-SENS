61e5a815779c3f1b52cb1a311c01e5a4be46f3a9
HTMLhead <mrdoo309@gmail.com>
2019-05-17 19:16:47 +0900

    Feat: main로딩 애니메이션 추가

---
+++ src/component/presenter/loaders/MainLoadingAni.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const MainLoadingAni = styled.div`
+  width: 50%;
+  margin: auto;
+  height: 50vh;
+  background-image: url('./image/mainLoading.gif');
+`;
+
+export default MainLoadingAni;

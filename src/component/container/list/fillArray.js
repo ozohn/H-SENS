cf60d1e07cfc7ad8800d1ea96efe4118fd63544c
pdvonzoo <vonzoo@naver.com>
2019-05-27 18:33:55 +0900

    Refactoring: List refactoring 및 apollo 적용

--- src/component/container/list/fillArray.js
+++ src/component/container/list/fillArray.js
@@ -1,12 +1,15 @@
-const fillArray = (arr, num) => {
+const fillArray = ({ arr, num, targetNum }) => {
   if (arr === undefined) return [];
-  const checkingArr = [...arr];
-  let i = 0;
-  while (checkingArr.length < num) {
-    checkingArr.push({ _id: i });
-    i += 1;
+  let depth = -1;
+  const newArr = [];
+  for (let i = 0; i < num; i++) {
+    if (!(i % targetNum)) {
+      depth += 1;
+      newArr.push([]);
+    }
+    newArr[depth].push(arr[i] ? arr[i] : null);
   }
-  return checkingArr;
+  return newArr;
 };
 
 export default fillArray;

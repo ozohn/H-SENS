8ab1729c45d0da4ee6a2db8c4de4a95c2049d354
pdvonzoo <vonzoo@naver.com>
2019-05-27 18:39:42 +0900

    Refactoring: 사용하지 않는 모듈 제거

--- src/component/container/list/List.jsx
+++ src/component/container/list/List.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable no-underscore-dangle */
-// HTTP method
-// restfulUrl best practice;, restAPI
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { gql } from 'apollo-boost';

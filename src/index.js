3275f710fdb7440d6d62fce7fc4d22d1444edd05
HTMLhead <mrdoo309@gmail.com>
2019-05-29 11:22:17 +0900

    Refactor: serviceWorker제거

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo-hooks';
 import App from './App';
 import Client from './apollo/Client';
-import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
@@ -14,8 +13,3 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
